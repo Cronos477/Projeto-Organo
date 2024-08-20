89e1eb96bb87884b1c678e0db9fa87f765070b4b
Cronos477 <felipe.chess47@gmail.com>
2024-08-20 17:02:01 -0300

    0.0.01

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export default function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal da Organo"/>
+        </header>
+    )
+}
+
